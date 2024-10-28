c8f3952cd16948a52dd5a6ba8738e9d3a4e8bdab
lucag322 <97728486+lucag322@users.noreply.github.com>
2024-10-28 22:50:12 +0100

    feat: add trace ray class

--- main.cpp
+++ main.cpp
@@ -2,43 +2,53 @@
 #include "Color.hpp"
 #include "Image.hpp"
 #include "Uinputs.hpp"
+#include "Vec3.hpp"
+#include "Raytrace.hpp"
 
 using namespace std;
 
 
 int main()
 {    
-    UserInputs userInputs;
-    
-    Color red(1, 0, 0);
-    Color black;
 
-    cout << "Red : " << red << std::endl;
-    cout << "Black : " << black << std::endl;
+    UserInputs userInputs;
 
     int WIDTH = userInputs.AskIntInput("Enter the width of the image : ");
     int HEIGHT = userInputs.AskIntInput("Enter the height of the image : ");
 
-    Image image(WIDTH, HEIGHT, black);
+    auto focal_length = userInputs.AskFloatInput("Enter the focal length of the camera : ");
+    Image image(WIDTH, HEIGHT, Color(0, 0, 0));
+    auto viewport_height = 2;
+    auto viewport_width = 2;
+    auto camera_center = point3(0, 0, 0);
 
-    int centerX = WIDTH / 2;
-    int centerY = HEIGHT / 2;
-    int radius = min(WIDTH, HEIGHT) / 4;
-    int currentPercentage = 0;
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, viewport_height, 0);
 
-    for (int y = 0; y < HEIGHT; y++) {
-        if ((y * 100) / HEIGHT > currentPercentage) {
-            std::cout << "\033[2J\033[1;1H";
-            currentPercentage = (y * 100) / HEIGHT;
-            std::cout << "Percentage calculated : " << (y * 100) / HEIGHT << "%" << std::endl;
-        }
+    auto pixel_delta_u = viewport_u / viewport_width;
+    auto pixel_delta_v = viewport_v / viewport_height;
 
-        for (int x = 0; x < WIDTH; x++) {
-            if ((x - centerX) * (x - centerX) + (y - centerY) * (y - centerY) < radius * radius) {
-                image.SetPixel(x, y, red);
-            }
+    auto viewport_uper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+    auto pixel_0_0_loc = viewport_uper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    std::cout << "Pixel 0,0 location : " << pixel_0_0_loc << std::endl;
+    std::cout << "image size :" << WIDTH << " x " << HEIGHT << std::endl;
+
+    for(int y = 0; y < HEIGHT; y++) {
+        std::cout << "\033[2J\033[1;1H";
+        std::cout << "Percentage calculated : " << (y * 100) / HEIGHT << "%" << std::endl;
+        for(int x = 0; x < WIDTH; x++) {
+            auto pixel_center = pixel_0_0_loc + pixel_delta_u * x + pixel_delta_v * y;
+            auto ray_direction = pixel_center - camera_center;
+            Raytrace r(camera_center, ray_direction);
+            std::cout << ray_direction << std::endl;
+
+            Color color_instance;
+            Color pixel_color = color_instance.ray_color(r);
+            image.SetPixel(x, y, pixel_color);
         }
     }
-    
-    image.WriteFile("../../../circle.png");
-}
+
+    std::cout << "Image saved to disk" << std::endl;
+    image.WriteFile("../../../circle.png"); 
+    }
--- src/raymath/Color.cpp
+++ src/raymath/Color.cpp
@@ -23,6 +23,17 @@ Color& Color::operator=(const Color& col) {
     return *this;
 }
 
+Color Color::operator*(double scalar) const {
+    return Color(color.x() * scalar, color.y() * scalar, color.z() * scalar);
+}
+
+Color Color::ray_color(const Raytrace& r) const {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+
+    return Color(1.0, 1.0, 1.0) * (1.0 - t) + Color(0.5, 0.7, 1.0) * t;
+}
+
 std::ostream& operator<<(std::ostream& _stream, const Color& col) {
     return _stream << "(" << col.R() << ", " << col.G() << ", " << col.B() << ")";
 }
