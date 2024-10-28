8bf2bed9d2ebcd05eaf132704c52a6935fb6b796
Dest.Com <37459465+DestroyCom@users.noreply.github.com>
2024-10-28 14:03:47 +0100

    feat: percentage loader

--- main.cpp
+++ main.cpp
@@ -24,8 +24,15 @@ int main()
     int centerX = WIDTH / 2;
     int centerY = HEIGHT / 2;
     int radius = min(WIDTH, HEIGHT) / 4;
+    int currentPercentage = 0;
 
     for (int y = 0; y < HEIGHT; y++) {
+        if ((y * 100) / HEIGHT > currentPercentage) {
+            std::cout << "\033[2J\033[1;1H";
+            currentPercentage = (y * 100) / HEIGHT;
+            std::cout << "Percentage calculated : " << (y * 100) / HEIGHT << "%" << std::endl;
+        }
+
         for (int x = 0; x < WIDTH; x++) {
             if ((x - centerX) * (x - centerX) + (y - centerY) * (y - centerY) < radius * radius) {
                 image.SetPixel(x, y, red);
